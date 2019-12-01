b2df4fb5a66a8b6cd9acbbbf88d03a1592d84586
Jeffacy99 <mwaniajapheth1999@gmail.com>
2019-12-01 21:04:43 +0300

    added generate array

--- src/test.ts
+++ src/test.ts
@@ -18,3 +18,50 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+/* Get DOM elements */
+const firstGeneratorBtn = document.querySelector('.firstGeneratorBtn');
+const firstGenerator = document.querySelector('.firstGenerator');
+
+
+/* First Generator */
+const verbArray = [
+  "sleeping",
+  "rolling",
+  "eating Pho",
+  "playing Mario",
+  "watching Netflix",
+  "doodling"
+];
+
+const placeArray = [
+  "on the top of Mt.Fuji",
+  "on the top of Eiffel Tower",
+  "on the beach",
+  "on the Moon",
+  "inside Space Shuttle"
+];
+
+const withWhoArray = [
+  "thirty kittens",
+  "twenty puppies",
+  "fifty rabbits",
+  "moose",
+  "bear",
+  "flock of sheep",
+  "pack of wolves"
+];
+
+const generateQuote = () => {
+  let x = Math.floor(Math.random() * verbArray.length);
+  let y = Math.floor(Math.random() * placeArray.length);
+  let z = Math.floor(Math.random() * withWhoArray.length);
+
+  return "Kaoru is " + verbArray[x] + " " + placeArray[y] + " with " + withWhoArray[z] + ".";
+};
+
+const setGenerator = () => {
+  firstGenerator.textContent = generateQuote();
+};
+
+
+firstGeneratorBtn.addEventListener('click', setGenerator);
